5378ff045f6ad2ae3f06464b2e517c3fd7c1fbf4
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2021-10-20 23:32:57 +0500

    backend development with Node Js

---
+++ Tutorial 1/main.js
@@ -0,0 +1,40 @@
+// Anonymous Function.........
+var person = function () {
+    console.log("this is Anonymous Function")
+}
+person();
+
+// arrow Function..........
+var user = ()=> console.log("this is arrow function")
+user();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
